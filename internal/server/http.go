28ce1b133d18956f0d2d2e620bb2043a2a5e3bde
OpenSauce <lawleycobhc@gmail.com>
2022-03-14 21:17:18 +0000

    JSON Restful API

---
+++ internal/server/http.go
@@ -0,0 +1,27 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type httpServer struct {
+	Log *Log
+}
+
+func NewHTTPServer(addr string) *http.Server {
+	httpsrv := newHTTPServer()
+	r := mux.NewRouter()
+	r.HandleFunc("/", httpsrv.handleProduce).Methods("POST")
+	return &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+}
+
+func newHTTPServer() *httpServer {
+	return &httpServer{
+		Log: NewLog(),
+	}
+}
